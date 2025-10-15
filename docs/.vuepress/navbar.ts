f4faa154468338e29fbd7e442228f035fb92d100
vpn04 <270612770@qq.com>
2025-10-15 12:59:00 +0000

    首页删除，youyou博客

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -6,9 +6,8 @@
 
 import { defineNavbarConfig } from 'vuepress-theme-plume'
 
-export default defineNavbarConfig([
-  { text: '首页', link: '/' },
-  { text: '博客', link: '/blog/' },
+export default defineNavbarConfig([ 
+  { text: 'YouYou博客', link: '/blog/' },
   { text: '标签', link: '/blog/tags/' },
   { text: '友链' , link: '/friends/'},
   { text: '归档', link: '/blog/archives/' },

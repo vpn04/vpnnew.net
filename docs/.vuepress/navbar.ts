4574340af3b38601aad2589bf5880d2eb6e3831b
vpn04 <270612770@qq.com>
2026-03-31 19:08:01 +0800

    新增标签

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -11,13 +11,14 @@ export default defineNavbarConfig(
     { text: '🛩机场排名汇总', link: '/vpn-recommend/' },
     { text: '💰机场免费试用汇总', link: '/article/mianfeijichang/' },
     { text: '💹机场优惠券汇总', link: '/article/youhuijuan/' },
+    { text: '💡 Apple ID 共享账号', link: '/article/Shadowrocket/' },
     { text: '📁YouYou博客', link: '/blog/' },
     { 
       text: '📂更多',
       items: [
-        { text: '标签', link: '/blog/tags/' },
-        { text: '友链' , link: '/friends/'},
-        { text: '归档', link: '/blog/archives/' },
+        { text: '🏷️标签', link: '/blog/tags/' },
+        { text: '🔗友链' , link: '/friends/'},
+        { text: '💼归档', link: '/blog/archives/' },
       ]
     }
   ]
